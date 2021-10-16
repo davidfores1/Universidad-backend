2f4fe8f7c5181885be09ac0128405b1f8df65213
David Forero <davidforero717@gmail.com>
2021-10-16 11:03:25 -0500

    inicio proyecto universidad-backend en spring boot y mysql(api)

---
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
@@ -0,0 +1,14 @@
+package com.springsimplespasos.universidad.universidadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadBackendApplication.class, args);
+		System.out.println("Hola Mundo desde springBoot");
+	}
+
+}
