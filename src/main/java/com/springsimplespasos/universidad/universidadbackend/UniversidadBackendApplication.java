cf2073356df7b591b274b20a7aae1c5525071247
David Forero <davidforero717@gmail.com>
2021-10-16 11:37:58 -0500

    se realiza configuracion de conexion a base de datos en el archivo application.properties

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
@@ -8,7 +8,7 @@ public class UniversidadBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversidadBackendApplication.class, args);
-		System.out.println("Hola Mundo desde springBoot");
+
 	}
 
 }
